d6d42a6d124ba5744596b5bac6c6c02ac5eaf6a1
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2024-04-16 19:37:47 +0530

    assests added

---
+++ app/root/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">Events</h1>
+      <Button variant="destructive" className="danger">Hello World!</Button>
+    </main>
+    
+  );
+}
