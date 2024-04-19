f7040f9fbbffc5f10edece5042862dd53169dbe9
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2024-04-19 14:58:30 +0530

    Added MobileNav with Sheet, NavItems, and adjusted header/footer

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,9 @@
-import { SignedOut } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "../ui/button";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -9,14 +11,24 @@ const Header = () => {
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image 
-            src="/assets/images/logo.svg"
+            src="/assets/images/logo1.svg"
             width={128}
             height={38}
             alt="EventHub Logo"
           />  
         </Link>
 
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
               <Link href="/sign-in">
