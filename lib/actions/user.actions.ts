3ebb0c05c3f9fc83ffb15ce7b86e681027551db9
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2024-04-21 04:44:36 +0530

    mplemented webhook handling for user models and integrated Svix

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,78 @@
+'user server'
+
+import { revalidatePath } from 'next/cache';
+
+import { connectToDatabase } from '../database/index';
+
+import { CreateCategoryParams, UpdateUserParams } from '../../types/index';
+
+import { handleError } from '../utils';
+import User from '../database/models/user.model';
+import Event from '../database/models/event.model';
+import Order from '../database/models/order.model'
+
+export async function createUser(user: CreateCategoryParams) {
+    try {
+        await connectToDatabase();
+
+        const newUser = await User.create(user);
+
+        return JSON.parse(JSON.stringify(newUser));
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+export async function getUserById(userId: string) {
+    try {
+        await connectToDatabase()
+
+        const user = await User.findById(userId)
+
+        if (!user) throw new Error('User not found')
+        return JSON.parse(JSON.stringify(user))
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+export async function updateUser(clerkId: string, user: UpdateUserParams) {
+    try {
+        await connectToDatabase()
+
+        const updateUser = await User.findOneAndUpdate({ clerkId }, user, { ne: true })
+
+        if (!updateUser) throw new Error('User update failed') 
+        return JSON.parse(JSON.stringify(updateUser))
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+export async function deleteUser(clerkId: string) {
+    try {
+        await connectToDatabase()
+
+        const userToDelete = await User.findOne({ clerkId })
+
+        if (!userToDelete) {
+            throw new Error('User not found')
+        }
+
+        await Promise.all([
+            Event.updateMany(
+                { _id: { $in: userToDelete.events } },
+                { $pull: { organizer: userToDelete } }
+            ),
+
+            Order.updateMany({ _id: { $in: userToDelete.orders  } }, { $unset: { buyer: 1 } })
+        ])
+
+        const deletedUser = await User.findByIdAndDelete(userToDelete._id)
+        revalidatePath('/')
+
+        return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
+    } catch (error) {
+        handleError(error)
+    }
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,90 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import queryString from 'query-string';
+import { UrlQueryParams, RemoveUrlQueryParams } from '@/types';
+
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const formatDateTime = (dateString: Date) => {
+  const dateTimeOptions: Intl.DateTimeFormatOptions = {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+  }
+
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: 'short',
+    month: 'short',
+    year: 'numeric',
+    day: 'numeric'
+  }
+  
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+  }
+
+  const formattedDateTime: string = new Date(dateString).toLocaleString('en-US', dateTimeOptions)
+  const formattedDate: string = new Date(dateString).toLocaleString('en-US', dateOptions)
+  const formattedTime: string = new Date(dateString).toLocaleDateString('en-US', timeOptions)
+
+  return {
+    dateTime: formatDateTime,
+    dateOnly: formattedDate,
+    timeOnly: formattedTime
+  }
+}
+
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file)
+
+export const formatPrice = (price: string) => {
+  const amount = parseFloat(price)
+  const formattedPrice = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'INR',
+  }).format(amount)
+
+  return formattedPrice
+}
+
+export function formUrlQuery({ params, key, value }: UrlQueryParams) {
+  const currentUrl = queryString.parse(params)
+
+  currentUrl[key] = value
+
+  return queryString.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  )
+}
+
+export function removeKeysFromQuery({ params, keysToRemove }: RemoveUrlQueryParams) {
+  const currentUrl = queryString.parse(params)
+
+  keysToRemove.forEach(key => {
+    delete currentUrl[key]
+  })
+
+  return queryString.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  )
+}
+
+export const handleError = (error: unknown) => {
+  console.error(error);
+  throw new Error(typeof error === 'string' ? error : JSON.stringify(error))
 }
