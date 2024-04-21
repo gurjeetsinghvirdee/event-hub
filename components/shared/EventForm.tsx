7f30b6d0543336278086921ab03a3f6da20f08c0
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2024-04-22 02:41:50 +0530

    Implemented event form with UI integration, auth, and Zod schema validation

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from "zod";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const orangeButton = {
+    background: "linear-gradient(to right, #ff4903, #FF7700, #FF8811)",
+    color: "#fff"
+};
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters long."
+    })
+})
+
+type EventFormProps = {
+    userId: string
+    type: "Create" | "Update"
+}
+
+const EventForm = ({ userId, type }: EventFormProps) => {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field}) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="username" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public display name
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit" style={orangeButton}>Submit</Button>
+            </form>
+        </Form>
+    )
+}
+
+export default EventForm;
