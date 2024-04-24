bd21432bcd9c5418e7364003c5073d23e0a7c1a9
Gurjeet Singh Virdee <gurjeetsinghvirdee@users.noreply.github.com>
2024-04-24 17:01:20 +0530

    feat: New Categories dropdown Implemented

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { useState, startTransition } from 'react';
+import { useState, startTransition, useEffect } from 'react';
 import { ICategory } from '../../lib/database/models/category.model';
 import {
     Select,
@@ -19,6 +19,7 @@ import {
     AlertDialogTrigger
 } from "@/components/ui/alert-dialog";
 import { Input } from '../ui/input';
+import { createCategory, getAllCategories } from '@/lib/actions/category.action';
 
 type DropdownProps = {
     value?: string
@@ -29,9 +30,25 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
 
     const [categories, setCategories] = useState<ICategory[]>([]);
     const [newCategory, setNewCategory] = useState(``);
+
     const handleAddCategory = async () => {
-        
+        createCategory({
+          categoryName: newCategory.trim()
+        })
+        .then((category) => {
+          setCategories((prevState) => [...prevState, category])
+        })
     }
+
+    useEffect(() => {
+      const getCategories = async () => {
+        const categoryList = await getAllCategories();
+
+        categoryList && setCategories(categoryList as ICategory[]);
+      }
+
+      getCategories();
+    }, [])
 
     return (
       <Select onValueChange={onChangeHandler} defaultValue={value}>
--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -270,6 +270,8 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                                                             <Checkbox 
                                                                 id="isFree"
                                                                 className="mr-2 h-5 w-5 border-2 border-primary-100 mb-1"
+                                                                onCheckedChange={field.onChange}
+                                                                checked={field.value}
                                                                 style={checkBox}
                                                             />
                                                         </div>
