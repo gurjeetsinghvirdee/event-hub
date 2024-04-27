e083141d0d3cb7ad1ba2acd2d31542d0b07ad94a
Gurjeet Singh Virdee <gurjeetsinghvirdee@users.noreply.github.com>
2024-04-27 16:49:34 +0530

    fixed: User model error

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -6,6 +6,7 @@ const CreateEvent = () => {
     const { sessionClaims } = auth();
 
     const userId = sessionClaims?.userId as string;
+    
 
     return (
         <>
--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -7,8 +7,8 @@ const UserSchema = new Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     photo: { type: String, required: true }
-})
+});
 
-const User = models.user || model('User', UserSchema); 
+const User = models.User || model('User', UserSchema); 
 
 export default User;
